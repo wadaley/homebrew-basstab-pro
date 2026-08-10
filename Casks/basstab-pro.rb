e24f300dd4d9210d74808b7a00c5a83ee75ae1d8
Basstab Pro CI <ci@basstab.pro>
2026-08-10 16:33:08 +0000

    Update basstab-pro to 0.2.410

--- Casks/basstab-pro.rb
+++ Casks/basstab-pro.rb
@@ -1,14 +1,14 @@
 cask "basstab-pro" do
-  version "0.2.406"
+  version "0.2.410"
 
   on_arm do
-    url "https://dev.basstab.pro/desktop/dev/mac-arm/bb86a20/Basstab-Pro-mac-arm.dmg"
-    sha256 "e6f0c683435ad6b21d22f22e94633d1772c03ee8e4343c40561af317a1cb3833"
+    url "https://dev.basstab.pro/desktop/dev/mac-arm/ee49878/Basstab-Pro-mac-arm.dmg"
+    sha256 "3ea0c027fe7d565c2ea4d30abb590e325e40e9cff98425ff1b799bb7fd3968ee"
   end
 
   on_intel do
-    url "https://dev.basstab.pro/desktop/dev/mac-intel/bb86a20/Basstab-Pro-mac-intel.dmg"
-    sha256 "d3cb0dfd5ea9e16f0e10231cf3a56343a556af145f1177621cc9d60e62a570a9"
+    url "https://dev.basstab.pro/desktop/dev/mac-intel/ee49878/Basstab-Pro-mac-intel.dmg"
+    sha256 "bb6095b7a3fa5bcc3d7a9cda036c143b06606ea62e4a1de9e17d69fd4592fe0d"
   end
 
   name "Basstab Pro"
